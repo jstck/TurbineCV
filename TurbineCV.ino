b3bdc36175a4d57e47c2c1b5be5832b1b55ba95b
John Stäck <stack@spotify.com>
2018-11-10 22:12:14 +0100

    Some refactoring and adding EMIv1 features

--- TurbineCV.ino
+++ TurbineCV.ino
@@ -14,49 +14,71 @@
 //arduinos where serial console does not use the same hardware serial port as MIDI
 #define DEBUG_PRINT
 
-#define N_CC 4
-
 #define UNUSED_CC 255
 #define HIRES_CC_AUTO 254
 
+#define N_DAC 6
+
 
 //MIDI CC messages used for the different outputs
-uint8_t cc_list[N_CC] = {
-  2,        //Breath
-  4,        //Foot pedal
-  5,        //Porta time
-  1,        //Mod wheel
+uint8_t cc_list[N_DAC] = {
+  UNUSED_CC,  //Note
+  5,          //Glide (portamento time)
+  UNUSED_CC,  //Pitch bend
+  2,          //Breath
+  1,          //Mod wheel
+  UNUSED_CC   //Velocity
 };
 
 //"DAC slots" where note pitch (v/oct) and velocity are sent. DAC 0 is slots 0 and 1, DAC 1 is 2 and 3, DAC 2 is 4 and 5.
-#define PITCH_SLOT 4
+#define NOTE_SLOT 0
+#define BEND_SLOT 2
 #define VELOCITY_SLOT 5
 
+#define AT_SLOT 3 //Aftertouch goes in breath slot
+
 //High-resolution CC messages. Typically "regular CC" + 32. Can be manually set here.
-//If set to HIRES_CC_AUTO, these are automatically assigned to CC+32 where appropriate.
+//If set to HIRES_CC_AUTO, these are automatically assigned to CC+32 (where main CC is in 1-31 range).
 //If hi-res is unwanted, set this to UNUSED_CC to block it off
-uint8_t cc_hires[N_CC] = {
+//Slots that are not used for CC's are disabled
+uint8_t cc_hires[N_DAC] = {
+  UNUSED_CC,
+  HIRES_CC_AUTO,
+  UNUSED_CC,
   HIRES_CC_AUTO,
   HIRES_CC_AUTO,
-  HIRES_CC_AUTO,
-  HIRES_CC_AUTO,
+  UNUSED_CC,
 };
 
 //Digital output pins for gate and trigger
-#define GATE_PIN 5
-#define TRIG_PIN 6
+#define TRIG_PIN 5
+#define GATE_PIN 6
+
+//Digital output pin for activity LED
+#define LED_PIN 10
+
+//Input pins for switches
+#define GLIDE_SWITCH          7 //SW2
+#define BEND_SWITCH           8 //SW3
+#define AFTERTOUCH_SWITCH     9 //SW4
 
 //How long to run the trigger pulse for (roughly) in microseconds
 #define TRIGGER_WIDTH 100
 
+//How long to turn the activity LED on for, in milliseconds
+#define MIDI_ACT_TIME 50
+
+//How often to read onboard switches
+#define SWITCH_READ_INTERVAL 500
+
 //Chip select pins for SPI bus to DACs. The board default pins are used for MOSI and SCK.
-uint8_t cs_pins[3] = {2, 3, 4};
+uint8_t cs_pins[N_DAC/2] = {2, 3, 4};
 
 //Flags to set high=1 (0-4V) or low=0 gain (0-2V) per DAC.
-uint8_t dac_gain[6] = {0, 0, 0, 0, 0, 0};
+uint8_t dac_gain[N_DAC] = {1, 0, 0, 0, 0, 0};
 
 //Flags to invert DAC channel. Normal operation (0) is lowest voltage at 0 CC value / low note, 1 inverts this.
-uint8_t cv_invert[6] = {0, 0, 0, 0, 0, 0};
+uint8_t cv_invert[N_DAC] = {0, 0, 0, 0, 0, 0};
 
 
 //Map two 7-bit parts into one 12-bit value
@@ -67,9 +89,13 @@ bool trigger_on = false;
 int8_t current_note = NO_NOTE; //MIDI value of currently playing note
 uint8_t note_velocity = 0; //Velocity of currently playing note
 unsigned long trigger_on_time;
-int16_t pitch_bend = 0; //Signed value, -8192 - +8191
+int pitch_bend = 0; //Signed value, -8192 - +8191
 
 
+bool glide_merge;
+bool bend_merge;
+bool use_aftertouch;
+unsigned long switchesRead;
 
 
 //Do midi with default settings.
@@ -77,12 +103,12 @@ int16_t pitch_bend = 0; //Signed value, -8192 - +8191
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 //High and low bytes of each CC.
-uint8_t cc_msb[N_CC];
-uint8_t cc_lsb[N_CC];
+uint8_t cc_msb[N_DAC];
+uint8_t cc_lsb[N_DAC];
 
 
 //Keep track of wether or not we have seen a hi-res message for this CC.
-bool hires_seen[N_CC] = {false, false, false, false};
+bool hires_seen[N_DAC] = {false, false, false, false, false, false};
 
 void setup() {
 
@@ -93,28 +119,38 @@ void setup() {
 
   note_init();
 
-  //Initialise outputs
+  //Initialise in- and outputs
   pinMode(GATE_PIN, OUTPUT);
   digitalWrite(GATE_PIN, LOW);
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  pinMode(GLIDE_SWITCH, INPUT_PULLUP);
+  pinMode(BEND_SWITCH, INPUT_PULLUP);
+  pinMode(AFTERTOUCH_SWITCH, INPUT_PULLUP);
+
+  readSwitches();
   
   SPI.begin();
-  for(uint8_t i=0; i<3; ++i) {
+  for(uint8_t i=0; i<N_DAC/2; ++i) {
     pinMode(cs_pins[i], OUTPUT);
     digitalWrite(cs_pins[i], HIGH);
   }
-  for(uint8_t i=0; i<N_CC; ++i) {
+  for(uint8_t i=0; i<N_DAC; ++i) {
     cc_msb[i] = 0;
     cc_lsb[i] = 0;
-    sendCC(i);
+    if(cc_list[i] != UNUSED_CC) {
+      sendCC(i);
+    }
   }
 
   //Setup high-resolution CC
-  for(uint8_t i=0; i<N_CC; ++i) {
+  for(uint8_t i=0; i<N_DAC; ++i) {
     if(cc_hires[i] == HIRES_CC_AUTO) {
       uint8_t main_cc = cc_list[i];
-      if(main_cc<=31) {
+      if(main_cc >= 1 && main_cc<=31) {
         cc_hires[i] = main_cc+32;
       } else {
         //Set to auto but cannot be used
@@ -128,11 +164,12 @@ void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.setThruFilterMode(midi::Thru::Full);
 
-  //Handle things
+  //Handle MIDI things
   MIDI.setHandleControlChange(midiChangeControl);
   MIDI.setHandlePitchBend(midiPitchBend);
   MIDI.setHandleNoteOn(midiNoteOn);
   MIDI.setHandleNoteOff(midiNoteOff);
+  MIDI.setHandleAfterTouchChannel(midiAftertouchChannel);
 
 #ifdef DEBUG_PRINT
   Serial.println("TurbineCV ready");
@@ -152,6 +189,10 @@ void loop() {
       trigger_on = false;
     }
   }
+
+  if(millis() >= switchesRead + SWITCH_READ_INTERVAL) {
+    readSwitches();
+  }
 }
 
 //Callback for CC messages
@@ -168,8 +209,14 @@ void midiChangeControl(byte channel, byte number, byte value) {
 
   if(MIDI_CHANNEL && channel != MIDI_CHANNEL) return; //Only care about specific channel
 
-  for(uint8_t i=0; i<N_CC; ++i) {
+  for(uint8_t i=0; i<N_DAC; ++i) {
     if(number==cc_list[i]) {
+
+      //Ignore this value if aftertouch is used for this slot
+      if(use_aftertouch && i==AT_SLOT) {
+        return;
+      }
+
       //Read MSB ("low-res" part)
       if(value != cc_msb[i]) {
         if(hires_seen[i]) {
@@ -226,8 +273,13 @@ void midiPitchBend(byte channel, int bend) {
 
   if(MIDI_CHANNEL && channel != MIDI_CHANNEL) return; //Only care about specific channel
 
+  int pb_dac = (bend+8192) / 4; //Scale pb value from -8192 - 8191 down to "dac range", 0-4095
+  sendDac(BEND_SLOT, pb_dac);
   pitch_bend = bend;
-  play_note(false);
+
+  if(bend_merge) {
+    play_note(false);  
+  }
 }
 
 void midiNoteOn(byte channel, byte note, byte velocity) {
@@ -273,10 +325,33 @@ void midiNoteOff(byte channel, byte note, byte velocity) {
   //Leave last note playing, just mute it
   //current_note = note_off(note);
   //current_note = NO_NOTE;
+  if(note != current_note) return;
 
   note_velocity = 0;
   digitalWrite(GATE_PIN, LOW);
   sendVelocity(note_velocity);
+
+}
+
+void midiAftertouchChannel(byte channel, byte pressure) {
+
+  #ifdef DEBUG_PRINT
+  Serial.print("Chan: ");
+  Serial.print(channel);
+  Serial.print(" aftertouch: ");
+  Serial.println(pressure);
+  #endif
+  
+  //Bail out if not set to use aftertouch (via switch)
+  if(!use_aftertouch) {
+    return;
+  }
+
+  if(MIDI_CHANNEL && channel != MIDI_CHANNEL) return;
+
+  //Aftertouch is only low-resolution.
+  cc_msb[AT_SLOT] = pressure;
+  cc_lsb[AT_SLOT] = 0;
 
 }
 
@@ -285,7 +360,7 @@ void play_note(bool new_note) {
   //Send note V/oct
   uint16_t note_dac_value = cvtable[current_note-MIDI_NOTE_LOW];
 
-  if(pitch_bend != 0) {
+  if(bend_merge && pitch_bend != 0) {
     int pb_amount;
     if(pitch_bend > 0) { //pitch bend up
 
@@ -314,7 +389,7 @@ void play_note(bool new_note) {
   Serial.println(note_dac_value);
   #endif
 
-  sendDac(PITCH_SLOT, note_dac_value);
+  sendDac(NOTE_SLOT, note_dac_value);
 
   //Only do triggery things if it is a "new note" (i.e. not just pitch bend update)
   if(new_note) {
@@ -375,4 +450,12 @@ void sendDac(uint8_t slot, uint16_t value) {
   SPI.transfer(command & 0x00FF);
   digitalWrite(pin, HIGH);
   SPI.endTransaction();
+}
+
+//Read switches and stuff into global variables for easy access
+void readSwitches() {
+  glide_merge = digitalRead(GLIDE_SWITCH);
+  bend_merge = digitalRead(BEND_SWITCH);
+  use_aftertouch = digitalRead(AFTERTOUCH_SWITCH);
+  switchesRead = millis();
 }
